f8fb8b2d501b6bba160aa55ee33dc7916b21958b
YiShan8787 <59713684gx@gmail.com>
2019-11-10 21:37:32 +0800

    update

--- dbModel.py
+++ dbModel.py
@@ -4,8 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@server/db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@server/db"
 
 
 db = SQLAlchemy(app)
